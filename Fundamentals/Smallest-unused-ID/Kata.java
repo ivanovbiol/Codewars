d8be8e65d6357205bc86d777f7aadcc8d4d8f889
Ivan V. Ivanov <ivanov.biol@gmail.com>
2021-01-27 17:28:27 +0200

    Task solved

---
+++ Fundamentals/Smallest-unused-ID/Kata.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Kata {
+
+    public static int nextId(int[] ids) {
+
+        List<Integer> integerList = new ArrayList<>();
+
+        Arrays.stream(ids).forEach(e -> {
+            if (!integerList.contains(e)) {
+                integerList.add(e);
+            }
+        });
+
+        Collections.sort(integerList);
+
+        return integerList.size() == 0 ? 0 : findSmallestId(integerList);
+
+    }
+
+    public static int findSmallestId(List<Integer> integerList) {
+
+        int counter = 0;
+
+        for (int i = 0; i < integerList.size(); i++) {
+            if (counter != integerList.get(i)) {
+                return counter;
+            }
+            counter++;
+        }
+
+        return integerList.get(integerList.size() - 1) + 1;
+    }
+
+}
+
