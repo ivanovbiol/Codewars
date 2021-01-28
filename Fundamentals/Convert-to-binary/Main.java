ad4dc9147bc652a8f28d6c59cf4a0ccf9de9bb0f
Ivan V. Ivanov <ivanov.biol@gmail.com>
2021-01-28 11:44:45 +0200

    Task solved

---
+++ Fundamentals/Convert-to-binary/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(Kata.toBinary(number));
+
+    }
+
+}
