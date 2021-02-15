85263ab3a48c8169d25bf3a2c1b632ad1865f363
Ivan V. Ivanov <ivanov.biol@gmail.com>
2021-02-15 12:36:46 +0200

    Task updated

--- 8-kyu/fake-binary/FakeBinary.java
+++ 8-kyu/fake-binary/FakeBinary.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class FakeBinary {
+
+    public static void main(String[] args) {
+
+        String numberString = readInputString();
+
+        System.out.println(fakeBin(numberString));
+
+    }
+
+    public static String readInputString() {
+
+        return new Scanner(System.in).nextLine();
+
+    }
 
     public static String fakeBin(String numberString) {
 
