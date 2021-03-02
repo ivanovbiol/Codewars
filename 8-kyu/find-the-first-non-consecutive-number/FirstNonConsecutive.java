ea6b4de9cbc43d89eddfbea9637f7a121ba1b16a
Ivan V. Ivanov <ivanov.biol@gmail.com>
2021-03-02 12:45:47 +0200

    Task updated with Test class

--- 8-kyu/find-the-first-non-consecutive-number/FirstNonConsecutive.java
+++ 8-kyu/find-the-first-non-consecutive-number/FirstNonConsecutive.java
@@ -1,16 +1,12 @@
 public class FirstNonConsecutive {
 
     static Integer find(final int[] array) {
-
         for (int i = 1; i < array.length; i++) {
 
             if (array[i] - array[i - 1] > 1) {
-
-                return Integer.valueOf(array[i]);
-
+                return array[i];
             }
         }
-
         return null;
     }
 }
